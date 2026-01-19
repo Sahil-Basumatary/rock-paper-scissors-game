7ffa4793bc23938ef76c6a53002bb7c80cc59a66
Sahil Basumatary <Basumatarysahil03@gmail.com>
2026-01-19 01:39:02 +0000

    feat(backend): add spring boot main class

---
+++ backend/src/main/java/com/rpsarena/RpsArenaApplication.java
@@ -0,0 +1,12 @@
+package com.rpsarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsArenaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RpsArenaApplication.class, args);
+    }
+}
+
